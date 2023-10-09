db595cf2c44069a5e2a3f4b1b077e6b99c002843
Ashley Simon <ashleyjsimon@yahoo.com>
2023-10-09 12:17:56 -0500

    acc prices in car

--- src/Cart.js
+++ src/Cart.js
@@ -1,28 +1,36 @@
 import React from 'react';
 
-const Cart = ({ updateOrder, removeFromCart, lineItems, cart, products })=> {
+const Cart = ({ updateOrder, removeFromCart, lineItems, cart, products }) => {
   return (
     <div>
       <h2>Cart</h2>
       <ul>
-        {
-          lineItems.filter(lineItem=> lineItem.order_id === cart.id).map( lineItem => {
-            const product = products.find(product => product.id === lineItem.product_id) || {};
+        {lineItems
+          .filter((lineItem) => lineItem.order_id === cart.id)
+          .map((lineItem) => {
+            const product =
+              products.find((product) => product.id === lineItem.product_id) ||
+              {};
             return (
-              <li key={ lineItem.id }>
-                { product.name }
-                ({ lineItem.quantity })(${((product.price * lineItem.quantity) / 1).toFixed(2)})
-                <button onClick={ ()=> removeFromCart(lineItem)}>Remove From Cart</button>
+              <li key={lineItem.id}>
+                {product.name}({lineItem.quantity})($
+                {((product.price * lineItem.quantity) / 1).toFixed(2)})
+                <button onClick={() => removeFromCart(lineItem)}>
+                  Remove From Cart
+                </button>
               </li>
             );
-          })
-        }
+          })}
       </ul>
-      {
-        lineItems.filter(lineItem => lineItem.order_id === cart.id ).length ? <button onClick={()=> {
-          updateOrder({...cart, is_cart: false });
-        }}>Create Order</button>: null
-      }
+      {lineItems.filter((lineItem) => lineItem.order_id === cart.id).length ? (
+        <button
+          onClick={() => {
+            updateOrder({ ...cart, is_cart: false });
+          }}
+        >
+          Create Order
+        </button>
+      ) : null}
     </div>
   );
 };
