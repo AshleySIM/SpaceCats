f0609f0e2147bbff55f03e83ac03043d91ff434a
Jonathan Lara <vaporycoder@Jonathans-MBP.attlocal.net>
2023-10-12 13:16:16 -0500

    passed in bookmarks to products page with no logged in user

--- src/Products.js
+++ src/Products.js
@@ -1,38 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Products = ({ products, cartItems, createLineItem, updateLineItem, auth, bookmarks})=> {
+const Products = ({
+  products,
+  cartItems,
+  createLineItem,
+  updateLineItem,
+  auth,
+  bookmarks,
+}) => {
   return (
     <div>
       <h2>Products</h2>
       <ul>
-        {
-          products.map( product => {
-            const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
-            const bookmark = bookmarks.find(bookmark => bookmark.product_id === product.id);
-            return (
-              <li key={ product.id }>
-                { product.name }
-                ${product.price}
-                {product.description}
-                {
-                  auth.id ? (
-                    cartItem ? <button onClick={ ()=> updateLineItem(cartItem)}>Add Another</button>: <button onClick={ ()=> createLineItem(product)}>Add</button>
-                  ): null 
-                }
-                {auth.id ? (
-                bookmark ? <button>Remove Bookmark</button>: <button>Bookmark</button>
-                ): null
-                }
-                {
-                  auth.is_admin ? (
-                    <Link to={`/products/${product.id}/edit`}>Edit</Link>
-                  ): null
-                }
-              </li>
-            );
-          })
-        }
+        {products.map((product) => {
+          const cartItem = cartItems.find(
+            (lineItem) => lineItem.product_id === product.id
+          );
+          const bookmark = bookmarks.find(
+            (bookmark) => bookmark.product_id === product.id
+          );
+          return (
+            <li key={product.id}>
+              {product.name}${product.price}
+              {product.description}
+              {auth.id ? (
+                cartItem ? (
+                  <button onClick={() => updateLineItem(cartItem)}>
+                    Add Another
+                  </button>
+                ) : (
+                  <button onClick={() => createLineItem(product)}>Add</button>
+                )
+              ) : null}
+              {auth.id ? (
+                bookmark ? (
+                  <button>Remove Bookmark</button>
+                ) : (
+                  <button>Bookmark</button>
+                )
+              ) : null}
+              {auth.is_admin ? (
+                <Link to={`/products/${product.id}/edit`}>Edit</Link>
+              ) : null}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
