6ffda1ae824f4a1e5e2f8d1dd2b94288c106f311
Jonathan Lara <vaporycoder@Jonathans-MBP.attlocal.net>
2023-10-17 19:59:41 -0500

    changed nav so it doesnt show the count of each component

--- src/index.js
+++ src/index.js
@@ -121,18 +121,19 @@ const App = () => {
     <div>
       {auth.id ? (
         <>
+        <h1 className="title">Space Cats</h1>
           <nav>
             <span>
               Welcome {auth.username}!<button onClick={logout}>Logout</button>
             </span>
-            <Link to="/products">Products ({products.length})</Link>
+            <Link to="/products">Products</Link>
             <Link to="/orders">
-              Orders ({orders.filter((order) => !order.is_cart).length})
+              Orders
             </Link>
 
             <Link to="/cart">Cart ({cartCount})</Link>
-            <Link to="/bookmarks">Bookmarks ({bookmarks.length})</Link>
-            <Link to="/reviews"> Reviews ({reviews.length}) </Link>
+            <Link to="/bookmarks">Bookmarks</Link>
+            <Link to="/reviews"> Reviews</Link>
           </nav>
           <main>
             <Routes>
