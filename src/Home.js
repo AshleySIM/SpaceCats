43e996bc5b7a5e08d4a125c5bb8a1daff53a4cf9
Jonathan Lara <vaporycoder@Jonathans-MBP.attlocal.net>
2023-10-18 21:06:08 -0500

    styled the home page a little more and changed the button colors

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from "react";
 const Home = ({}) => {
     return (
         <div className="homeDiv">
+            <div className="homeText">
             <h1>Welcome to Space Cats!</h1>
             <p>We are thrilled that you've chosen us as your destination for the latest and greatest "Space Cats" merchandise. At Space Cats, we're passionate about everything feline and space-related, and we've curated a collection of products that will make any cat lover's heart leap with joy!</p>
             <p>Whether you're looking for quirky cat-themed apparel, adorable accessories, or out-of-this-world cat toys, we've got you covered. Our mission is to bring a smile to your face with our unique and playful products.</p>
             <p>Thank you for joining our cosmic cat-loving community. Enjoy exploring our store, and we hope you find the purr-fect items that bring a little piece of the cosmos into your life!</p>
+            </div>
         </div>
     );
 };
