fea8a8a8e95648b42ce9ba635d52139da9d7e3d6
Jonathan Lara <vaporycoder@Jonathans-MBP.attlocal.net>
2023-10-18 20:38:09 -0500

    added home page

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = ({}) => {
     return (
-        <div>
+        <div className="homeDiv">
             <h1>Welcome to Space Cats!</h1>
             <p>We are thrilled that you've chosen us as your destination for the latest and greatest "Space Cats" merchandise. At Space Cats, we're passionate about everything feline and space-related, and we've curated a collection of products that will make any cat lover's heart leap with joy!</p>
             <p>Whether you're looking for quirky cat-themed apparel, adorable accessories, or out-of-this-world cat toys, we've got you covered. Our mission is to bring a smile to your face with our unique and playful products.</p>
